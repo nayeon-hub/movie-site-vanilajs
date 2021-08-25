e175b7d7d3e18c710cf16eb9be4eebabff243737
Park Na Yeon <within197@naver.com>
2021-08-25 23:29:28 +0900

    api 값 가져오기

---
+++ movieAPI.js
@@ -0,0 +1,12 @@
+const getUsers = () => {
+  axios
+    .get(
+      "https://api.themoviedb.org/3/movie/550?api_key=cdf3f17915090757a86f6a63885f852e"
+    )
+    .then((response) => {
+      const users = response.data;
+      console.log(`GET users`, users);
+    })
+    .catch((error) => console.error(error));
+};
+getUsers();
