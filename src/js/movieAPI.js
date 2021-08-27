227077f8415d9d9d2cc8319822764a6c10e485de
Park Na Yeon <within197@naver.com>
2021-08-27 19:29:57 +0900

    파일 이동

--- src/js/movieAPI.js
+++ src/js/movieAPI.js
@@ -1,7 +1,7 @@
 const getUsers = () => {
   axios
     .get(
-      "https://api.themoviedb.org/3/movie/550?api_key=cdf3f17915090757a86f6a63885f852e"
+      "https://api.themoviedb.org/3/genre/movie/list?api_key=cdf3f17915090757a86f6a63885f852e"
     )
     .then((response) => {
       const users = response.data;
