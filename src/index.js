eb633696a3355a5cc9633dec7e35765aba9fed1b
Park Na Yeon <within197@naver.com>
2021-08-30 00:58:54 +0900

    webpack 정리

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import header_top from "./js/components/header.js";
-import css from "./style.css";
+import "./css/index.css";
 
 let body = document.querySelector("body");
 body.append(header_top);
--- src/js/components/header.js
+++ src/js/components/header.js
@@ -68,4 +68,43 @@ header.className = "home-header";
 header.append(headerBox(headerTop()));
 header.append(headerBox(headerBottom()));
 
+// function showDrop() {
+//   const homeHeader = document.querySelector(".home-header");
+//   const miniNav = homeHeader.querySelector(".wrap-miniNav");
+//   const miniLists = miniNav.querySelectorAll("li");
+//   const overAnchor = miniLists[1].querySelector("a");
+//   const voucher = miniLists[1].querySelector(".voucher");
+
+//   // 이용권 메뉴 보여주기
+//   overAnchor.addEventListener("mouseover", () => {
+//     voucher.classList.remove("hiding");
+//     console.log(1);
+//   });
+
+//   voucher.addEventListener("mouseleave", () => {
+//     voucher.classList.add("hiding");
+//     console.log(2);
+//   });
+// }
+
+// function showSearch() {
+//   const homeHeader = document.querySelector(".home-header");
+//   const searchForm = homeHeader.querySelector("form");
+//   const searchInput = searchForm.querySelector("input");
+//   const searchBtn = homeHeader.querySelector(".search-btn");
+
+//   // 검색창 보여주기
+//   searchBtn.addEventListener("click", () => {
+//     searchForm.className = "";
+//   });
+
+//   searchInput.addEventListener("focusout", () => {
+//     searchForm.className = "hiding-search";
+//     console.log("hey");
+//   });
+// }
+
+// showSearch();
+// showDrop();
+
 export default header;
