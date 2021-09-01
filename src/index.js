d975a69e03aeb315846835ea095331f4befb12c1
Park Na Yeon <within197@naver.com>
2021-09-02 00:34:23 +0900

    라우터

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import header_list from "./js/components/header.js";
 import banner from "./js/components/main.js";
 import "./css/index.css";
 
+const navigateTo = (url) => {
+  history.pushState(null, null, url);
+  router();
+};
+
 const router = async () => {
   const routes = [
     { path: "/", view: banner },
@@ -26,20 +31,26 @@ const router = async () => {
       isMatch: true,
     };
   }
-  document.querySelector("#app").innerHTML = match.route.view;
+  document.querySelector("#app").innerHTML = await match.route.view;
 };
 
-// window.addEventListener("popstate", router);
+window.addEventListener("popstate", router);
 
 document.addEventListener("DOMContentLoaded", () => {
   // header
   let header = document.querySelector("header");
   header.className = "home-header";
-  header_list.forEach((ele) => header.append(ele));
+  header.innerHTML = header_list;
 
-  //banner
-  // let appDiv = document.getElementById("app");
-  // appDiv.innerHTML = banner;
-
+  document.body.addEventListener("click", (e) => {
+    if (e.target.matches("[data-link]")) {
+      e.preventDefault();
+      navigateTo(e.target.href);
+    }
+    if (e.target.parentNode.localName === "a") {
+      e.preventDefault();
+      navigateTo(e.target.parentNode.href);
+    }
+  });
   router();
 });
