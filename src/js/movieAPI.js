0e1d3a649b5771015f5a2296c1922ba8abb111f5
Park Na Yeon <within197@naver.com>
2021-09-01 18:49:24 +0900

    영화 데이터

--- src/js/movieAPI.js
+++ src/js/movieAPI.js
@@ -1,11 +1,21 @@
+const axios = require("axios");
+
 const getUsers = () => {
   axios
     .get(
-      "https://api.themoviedb.org/3/genre/movie/list?api_key=cdf3f17915090757a86f6a63885f852e"
+      "https://api.themoviedb.org/3/movie/upcoming?api_key=cdf3f17915090757a86f6a63885f852e&language=ko-KR&page=1"
     )
     .then((response) => {
-      const users = response.data;
-      console.log(`GET users`, users);
+      const movieLists = response.data.results;
+      Object.values(movieLists).forEach((ele) => {
+        console.log(
+          ele.title,
+          ele.id,
+          ele.genre_ids,
+          ele.overview,
+          ele.poster_path
+        );
+      });
     })
     .catch((error) => console.error(error));
 };
