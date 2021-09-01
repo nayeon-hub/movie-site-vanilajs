6e00fd29ca8e75bfbf8985ba3bdd02c6b3b9d8e9
Park Na Yeon <within197@naver.com>
2021-09-01 18:49:09 +0900

    로더와 plugin 추가

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const GoogleFontsPlugin = require("@beyonk/google-fonts-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -16,15 +18,38 @@ module.exports = {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.(png|svg|jpg|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "img/[name].[ext]?[hash]",
+            },
+          },
+        ],
+      },
     ],
   },
   devServer: {
     port: 9000,
+    historyApiFallback: true,
+    publicPath: "/dist/",
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
       filename: "./index.html",
     }),
+    new GoogleFontsPlugin({
+      fonts: [
+        {
+          family: "Noto Sans KR",
+          variants: ["500", "700"],
+          display: "block",
+        },
+      ],
+    }),
+    new CleanWebpackPlugin(),
   ],
 };
