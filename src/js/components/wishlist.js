914f63d9e365dcdad9727e8071a896e43de9b7b9
Park Na Yeon <within197@naver.com>
2021-09-22 22:48:05 +0900

    위시리스트 UI

---
+++ src/js/components/wishlist.js
@@ -0,0 +1,42 @@
+const axios = require("axios");
+import likeEvent from "../function/likeEvent.js";
+
+function miniMovie(title, path) {
+  const listDiv = document.querySelector(".wishList");
+  const movieItem = document.createElement("div");
+  movieItem.className = "movie-item";
+  const template = `
+    <div class="item-content">
+      <img src=https://image.tmdb.org/t/p/original/${path} alt="" />
+      <h2>${title}</h2>
+      <span class="like-btn unlike"></span>
+    </div>`;
+  movieItem.innerHTML = template;
+  listDiv.append(movieItem);
+}
+
+const getMovie = async (movieId) => {
+  axios
+    .get(
+      `https://api.themoviedb.org/3/movie/${movieId}?api_key=cdf3f17915090757a86f6a63885f852e&language=en-US`
+    )
+    .then((response) => {
+      const movie = response.data;
+      miniMovie(movie.title, movie.poster_path);
+      // likeEvent();
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
+
+export default () => {
+  const appDiv = document.getElementById("app");
+  appDiv.innerHTML = `<div class="wishList">
+  <h2>Movie 위시리스트</h2>
+  </div>`;
+  const wishList = JSON.parse(localStorage.getItem("movie"));
+  wishList.forEach((ele) => {
+    getMovie(ele);
+  });
+};
